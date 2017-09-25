0719987053878114546eeffd2c6443ef4ee460c4
Alexey Solodkiy <alexey.solodkiy@corp.badoo.com>
2017-09-25 13:31:38 +0300

    --save

--- rc.lua
+++ rc.lua
@@ -41,6 +41,11 @@ do
 end
 -- }}}
 
+function dump(data)
+    naughty.notify({ text = inspect(data) })
+end
+
+
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(awful.util.get_themes_dir() .. "doc/theme.lua")
@@ -346,8 +351,8 @@ globalkeys = awful.util.table.join(
               {description = "open a terminal", group = "launcher"}),
     awful.key({ modkey, "Control" }, "r", awesome.restart,
               {description = "reload awesome", group = "awesome"}),
-    awful.key({ modkey, "Shift"   }, "q", awesome.quit,
-              {description = "quit awesome", group = "awesome"}),
+    --awful.key({ modkey, "Shift"   }, "q", awesome.quit,
+    --           {description = "quit awesome", group = "awesome"}),
 --[[
 ]]
     
