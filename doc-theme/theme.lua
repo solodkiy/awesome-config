ea7b8eeeb394d627affacaa4c471c803dad6213f
Alexey Solodkiy <alexey.solodkiy@corp.badoo.com>
2018-08-09 13:05:32 +0300

    --save

--- doc-theme/theme.lua
+++ doc-theme/theme.lua
@@ -78,7 +78,7 @@ theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/def
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = "/opt/wallpapers/Tarantula_Nebula_Ultra_HD.jpg"
+theme.wallpaper = "/opt/wallpapers/Natural_Landscape_Alberta_Canada_4k.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
