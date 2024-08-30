919190ad8d5e2eea6efd793a21e5e9f799fca749
Alexey Solodkiy <alexey@solodkiy.by>
2024-08-31 00:17:08 +0300

    Rewrite mouse::enter

--- rc.lua
+++ rc.lua
@@ -703,11 +703,8 @@ end)
 
 -- Enable sloppy focus, so that focus follows mouse.
 client.connect_signal("mouse::enter", function(c)
-    if awful.layout.get(c.screen) ~= awful.layout.suit.magnifier
-        and awful.client.focus.filter(c) 
-        -- and c.class ~= 'jetbrains-phpstorm'
-        then
-            client.focus = c
+    if awful.layout.get(c.screen) ~= awful.layout.suit.magnifier and awful.client.focus.filter(c) then
+        client.focus = c
     end
 end)
 
